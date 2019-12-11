4487c626517adb98c26694262eb175aed76a1993
pedrocostaM <pedrocosta.m@outlook.pt>
2019-12-11 17:28:03 +0000

    star polygon

--- src/auxcallbacks.c
+++ src/auxcallbacks.c
@@ -93,7 +93,91 @@ void pari_draw_regularpolygon(GdkEvent *event)
  */
 void pari_draw_starpolygon(GdkEvent *event)
 {
-	//...
+	static int last_button=0;
+	GdkEventButton *geb=(GdkEventButton *)event; //aux variable
+	switch( event->type)
+	{
+		case GDK_BUTTON_PRESS: //it's a new one
+			last_button=geb->button;
+			if( geb->button == 1 )  //left button
+			{
+				double x=geb->x; 
+				double y=geb->y;
+				p_polygon *npoly=(p_polygon *)malloc(sizeof(p_polygon));
+				npoly->x=(double*)malloc((2*statusG.starpolygon_vertices)*sizeof(double));
+				npoly->y=(double*)malloc((2*statusG.starpolygon_vertices)*sizeof(double));
+				double ang=2*M_PI/statusG.starpolygon_vertices;
+				double radius=20;
+				double radius_m=10;
+
+				for(int n=0; n< 2*statusG.starpolygon_vertices;n++)
+				{   
+					if (n%2==0)
+					{
+						double x1=x+radius*cos( n*ang );
+						double y1=y+radius*sin( n*ang );
+						npoly->x[n]=x1;
+						npoly->y[n]=y1;
+					}
+					else
+					{
+						double x1=x+radius_m*cos( n*ang );
+						double y1=y+radius_m*sin( n*ang );
+						npoly->x[n]=x1;
+						npoly->y[n]=y1;
+					}
+					
+					
+					
+				}
+				npoly->num_points=2*statusG.starpolygon_vertices;  //to close the polygon
+				npoly->x0=x;
+				npoly->y0=y;
+				npoly->line_color=0;
+				npoly->fill_color=2*statusG.starpolygon_rgbcolor;
+
+				pari_add_poly_to_main_drawing(npoly, pdG);
+			}
+
+			break;
+
+		case GDK_MOTION_NOTIFY:  //mouse motion means modifying one just drawn
+			if( last_button == 1 )  //if last pressed button was the right one (avoid motion from other buttons)
+			{
+				double x=geb->x;
+				double y=geb->y;
+				double x0=pdG->polys[pdG->num_poly-1]->x0;
+				double y0=pdG->polys[pdG->num_poly-1]->y0;
+				p_polygon *npoly=pdG->polys[pdG->num_poly-1];
+				double ang=2*M_PI/statusG.starpolygon_vertices;
+				double radius=sqrt((x-x0)*(x-x0)+(y-y0)*(y-y0));
+				double radius_m=radius/2;
+				double angoff=atan2( y-y0, x-x0);
+				for(int n=0; n < 2*statusG.starpolygon_vertices;n++)
+				{
+					if (n%2==0)
+					{
+						double x1=x+radius*cos( n*ang );
+						double y1=y+radius*sin( n*ang );
+						npoly->x[n]=x1;
+						npoly->y[n]=y1;
+					}
+					else
+					{
+						double x1=x+radius_m*cos( n*ang);
+						double y1=y+radius_m*sin( n*ang );
+						npoly->x[n]=x1;
+						npoly->y[n]=y1;
+					}
+				}
+			}
+
+			break;
+
+		default:
+			//Does nothing
+			break;
+	}	
 }
 
 /**
